cdbf43ceb31ae90770c88498a1955444b56811ce
michael <michael.leyrer@yahoo.de>
2015-06-11 19:36:49 +0200

    Problem 20 solved

--- src/MAIN.java
+++ src/MAIN.java
@@ -9,7 +9,7 @@ public class MAIN {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Problem016.rechnen(2, 1000);
+		Problem020.rechnen(100);
 		//Problem007.rechnen(1000);
 	}
 
---
+++ src/Problem020.java
@@ -0,0 +1,32 @@
+
+import java.math.*;
+
+/**
+ * Created by michael on 6/11/15.
+ */
+
+//Get sum of digits of n!
+
+public class Problem020 {
+    static void rechnen(int n){
+
+        BigInteger num = BigInteger.ONE;
+        String numstring = "";
+        long sum = 0;
+
+
+        for(long i = n; i > 0; i--)
+            num = num.multiply(BigInteger.valueOf(i));
+
+        numstring += num;
+
+        char[] digits = numstring.toCharArray();
+
+        
+        for(int i = 0; i < digits.length; i++) {
+            sum += Integer.parseInt("" + digits[i]);
+        }
+
+        System.out.println(sum);
+    }
+}
